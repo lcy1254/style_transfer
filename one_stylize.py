7d9846324ec5df669e9cf9832a5e42181eda3367
Chaeyoung Lee <lcy12544@gmail.com>
2021-07-20 21:46:23 -0700

    update files

--- one_stylize.py
+++ one_stylize.py
@@ -1,3 +1,5 @@
+n = 15
+# need to start from 14
 import os, glob, json 
 import time
 import numpy as np
@@ -19,14 +21,19 @@ data_dir = '/media/dnr/data_crypt/cy_strap/camelyon17_v1.0/patches/'
 main_dir = '/media/dnr/data_crypt/cy_strap/train_folders' 
 main_outdir = '/media/dnr/data_crypt/cy_strap/output/'
 folders = glob.glob(f'{main_dir}/*/')
-
+'''
 for folder in folders:
-    print(folder)
     style_dir = folder
-    print(style_dir)
     type = str(folder.split('/')[-2]) + '_256_1024_256_alpha10.hdf5'
-    out_path = os.path.join(main_outdir, type)
-    print(out_path) 
+    out_path = os.path.join(main_outdir, type) 
     error = stylize_saveHdf5(data_dir=data_dir, style_dir=style_dir, out_path=out_path, alpha=1., save_size=256, content_size=1024, style_size=256)
     print(error)
+'''
 
+style_dir = folders[n]
+print(style_dir)
+type = str(folders[n].split('/')[-2]) + '_256_1024_256_alpha10.hdf5'
+out_path = os.path.join(main_outdir, type)
+print(out_path)
+error = stylize_saveHdf5(data_dir=data_dir, style_dir=style_dir, out_path=out_path, alpha=1., save_size=256, content_size=1024, style_size=256)
+print(error)
